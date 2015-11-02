5b5aa595b038886022868ccf6970456f933ad046
Houma Raheel <houmaraheel5@gmail.com>
2015-11-01 21:25:16 -0500

    assignment done

---
+++ services/user.js
@@ -0,0 +1,40 @@
+var userStore = require('../stores/user.js');
+
+module.exports.LinkApp = function(app) {
+	app.get('/api/users', function(req, res) {
+		userStore.getAll(function(user) {
+			res.json(user);
+		});
+	});
+
+	app.get('/api/users/:id', function(req, res) {
+		var listId = req.params.id;
+		userStore.get(listId, function(list, error) {
+			if (error) return res.sendStatus(400);
+			res.json(list);
+		});
+	});
+
+	app.post('/api/users', function(req, res) {
+		var list = req.body;
+		if (!list) return res.sendStatus(400);
+		userStore.save(list, function(list, error) {
+			if (error) return res.sendStatus(400);
+			res.json(list);
+		});
+	});
+
+	app.put('/api/users/:id', function(req, res) {
+		var listId = req.params.id;
+		var listUpdate = req.body;
+		if (!listUpdate) return res.sendStatus(400);
+		userStore.get(listId, function(list, error) {
+			if (error) return res.sendStatus(400);
+			listUpdate.id = list.id;
+			userStore.save(listUpdate, function(list, error) {
+				if (error) return res.sendStatus(400);
+				res.json(list);
+			});
+		});
+	});
+};
